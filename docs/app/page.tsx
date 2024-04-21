3ee28d70ddd3ba560c95feea4d997638a4c99abb
benny-nottonson <bennynottonson@gmail.com>
2024-04-21 14:01:16 -0700

    types

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,7 +1,4 @@
-import getDocs from "@/lib/docs";
-
 export default async function Home() {
-  console.log(await getDocs());
   return (
     <main>
     </main>
--- docs/lib/docs.ts
+++ docs/lib/docs.ts
@@ -1,7 +1,7 @@
+import { Documentation } from "@/types/mojo";
 import { exec } from "child_process";
 
-export default async function getDocs(): Promise<JSON> {
-    // exec mojo doc ../basalt
+export default async function getDocs(): Promise<Documentation> {
     return new Promise((resolve, reject) => {
         exec("mojo doc ../basalt", (error, stdout, stderr) => {
             if (error) {
