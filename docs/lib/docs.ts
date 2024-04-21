8b012fa9102247b7de3a490e3943480c3e09632e
benny-nottonson <bennynottonson@gmail.com>
2024-04-21 14:57:43 -0700

    added blob storage

--- docs/lib/docs.ts
+++ docs/lib/docs.ts
@@ -1,7 +1,8 @@
 import { Documentation } from "@/types/mojo";
 import { exec } from "child_process";
+import { del, list, put } from '@vercel/blob';
 
-export default async function getDocs(): Promise<Documentation> {
+export async function getDocs(): Promise<Documentation> {
     return new Promise((resolve, reject) => {
         exec("mojo doc ../basalt", (error, stdout, stderr) => {
             if (error) {
@@ -10,4 +11,31 @@ export default async function getDocs(): Promise<Documentation> {
             resolve(JSON.parse(stdout));
         });
     });
+}
+
+export async function loadDocs(): Promise<Documentation> {
+    const url = await getDocsUrl();
+    const response = await fetch(url);
+    return await response.json();
+}
+
+export async function saveDocs(docs?: Documentation) {
+    await deleteDocs();
+    const blob = new Blob([JSON.stringify(await getDocs())], { type: 'application/json' });
+    await put('docs', blob, { access: 'public' })
+}
+
+export async function getDocsUrl() {
+    const blobs = await list();
+
+    if (blobs.blobs.length === 0) {
+        await saveDocs();
+        return getDocsUrl();
+    }
+
+    return blobs.blobs[0].downloadUrl;
+}
+
+export async function deleteDocs() {
+    del((await list()).blobs.map(blob => blob.url));
 }
