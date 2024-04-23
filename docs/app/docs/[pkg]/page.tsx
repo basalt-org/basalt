08b86289d2a4a4799511283238a841fd0d786042
benny-nottonson <bennynottonson@gmail.com>
2024-04-23 09:03:37 -0700

    added sidebar

--- docs/app/docs/[pkg]/page.tsx
+++ docs/app/docs/[pkg]/page.tsx
@@ -1,11 +1,43 @@
 import Docs from "@/docs.json"
 
 export default function Package({ params }: { params: { pkg: string } }) {
-    const pkg = Docs.decl.packages.find((pkg) => pkg.name === params.pkg)
+    const CurrentPackage = Docs.decl.packages.find((pkg) => pkg.name === params.pkg);
+
+    const renderList = (title: string, items: any[], itemKey = 'name') => (
+        items.length > 0 && (
+            <div className="pl-4 py-2">
+                <h4 className="text-sm text-primary/90">{title}</h4>
+                <ul className="list-disc pl-5">
+                    {items.map((item) => (
+                        <li key={item[itemKey]}>
+                            {item.value ?
+                                <span className="text-primary/70">
+                                    {item[itemKey]} = {item.value}
+                                </span> :
+                                <a href={`#${item[itemKey]}`} className="text-primary/70 hover:text-primary/90">
+                                    {item[itemKey]}
+                                </a>
+                            }
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    );
 
     return (
         <main className="flex items-center justify-center pt-32">
-            
+            <aside className="w-80 h-screen fixed left-0 top-16 border-r border-gray-900 dark:border-gray-700 overflow-y-auto dark:text-white text-gray-900">
+                {CurrentPackage?.modules.map((mod) => (
+                    <div key={mod.name} className="border-y border-gray-300 dark:border-gray-800">
+                        <a href={`#${mod.name}`} className="block p-2 text-primary/90 hover:text-primary capitalize">{mod.name}</a>
+                        {renderList('Aliases', mod.aliases, 'name')}
+                        {renderList('Functions', mod.functions, 'name')}
+                        {renderList('Structs', mod.structs, 'name')}
+                    </div>
+                ))}
+                <div className="h-16" />
+            </aside>
         </main>
-    )
+    );
 }
