0bfdf8cf3cba9f0d7f9b14d7446f5be2e807d2f4
benny-nottonson <bennynottonson@gmail.com>
2024-04-25 12:51:59 -0700

    rework for breadcrumbs

--- docs/app/docs/[...pkg]/page.tsx
+++ docs/app/docs/[...pkg]/page.tsx
@@ -1,97 +1,41 @@
-import { Separator } from "@/components/ui/separator";
-import { ArrowLeft } from "lucide-react";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { findPackage } from "@/lib/docs";
-import ModuleItem from "@/components/module_item";
+import React, { Fragment, useMemo } from "react";
 
 export default function Package({ params }: { params: { pkg: string[] } }) {
-  const CurrentPackage = findPackage(params.pkg);
+  const pkg = findPackage(params.pkg);
 
-  if (!CurrentPackage) {
-    return (
-      <main className="flex items-center justify-center pt-32">
-        <div className="w-full max-w-6xl">
-          <h1 className="text-3xl font-semibold text-center">
-            Package not found
-          </h1>
-        </div>
-      </main>
-    );
+  if (!pkg) {
+    return <div>Package not found</div>;
   }
 
-  const hasModules =
-    CurrentPackage.modules && CurrentPackage.modules.length > 0;
-  const hasSubPackages =
-    CurrentPackage.packages && CurrentPackage.packages.length > 0; 
+  const generateHref = (index: number) => {
+    return `/docs/${params.pkg.slice(0, index + 1).join("/")}`;
+  };
+
+  const Crumbs = useMemo(() => {
+    return (
+      <Breadcrumb className="text-primary font-semibold p-4">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          {params.pkg.map((pkg, index) => (
+            <Fragment key={pkg}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href={generateHref(index)} className="capitalize">{pkg}</BreadcrumbLink>
+              </BreadcrumbItem>
+            </Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    );
+  }, [params.pkg]);
 
   return (
-    <main className="flex items-center justify-center pt-8">
-      <aside className="hidden md:block w-80 h-screen fixed left-0 top-16 border-r border-primary/20 overflow-y-auto dark:text-white text-gray-900">
-        {params.pkg.length > 1 ? (
-          <>
-            <a
-              href={`/docs/${params.pkg.slice(0, -1).join("/")}`}
-              className="block p-2 text-primary/90 hover:text-primary"
-            >
-              <ArrowLeft className={"inline-block -mt-1 mr-2 w-4 h-4"} />
-              Back
-            </a>
-            <Separator />
-          </>
-        ) : (
-          <>
-            <a
-              href="/"
-              className="block p-2 text-primary/90 hover:text-primary"
-            >
-              <ArrowLeft className={"inline-block -mt-1 mr-2 w-4 h-4"} />
-              Back
-            </a>
-            <Separator />
-          </>
-        )}
-
-        {hasModules && (
-          <h2 className="text-lg font-semibold p-2 text-primary/90">Modules</h2>
-        )}
-
-        {hasModules &&
-          CurrentPackage.modules!.map((mod) => (
-            <div key={mod.name}>
-              <a
-                href={`#${mod.name}`}
-                className="block p-2 pl-4 text-primary/90 hover:text-primary capitalize"
-              >
-                {mod.name}
-              </a>
-            </div>
-          ))}
-
-        {hasSubPackages && <Separator />}
-
-        {hasSubPackages && (
-          <h2 className="text-lg font-semibold p-2 text-primary/90">
-            Packages
-          </h2>
-        )}
-
-        {hasSubPackages &&
-          CurrentPackage.packages!.map((pkg) => (
-            <div key={pkg.name}>
-              <a
-                href={`/docs/${params.pkg.join("/")}/${pkg.name}`}
-                className="block p-2 pl-4 text-primary/90 hover:text-primary capitalize"
-              >
-                {pkg.name}
-              </a>
-            </div>
-          ))}
-        <div className="h-16" />
-      </aside>
-      <div className="w-full max-w-6xl flex flex-col gap-8">
-        {CurrentPackage.modules?.map((mod) => (
-          <ModuleItem key={mod.name} module={mod} />
-        ))}
-      </div>
+    <main>
+      {Crumbs}
     </main>
   );
 }
